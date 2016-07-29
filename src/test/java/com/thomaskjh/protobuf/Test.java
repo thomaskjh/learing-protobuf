21101b81e83c2cdf37474cab397a860f423feff1
thomaskjh <thomaskjh@qq.com>
2016-07-29 21:24:40 +0800

    learning-protobuf

---
+++ src/test/java/com/thomaskjh/protobuf/Test.java
@@ -0,0 +1,14 @@
+package com.thomaskjh.protobuf;
+
+public class Test {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
